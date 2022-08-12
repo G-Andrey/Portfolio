38757f222e2b8fd91d5e2a58067a6e9813aaf9b8
unknown <g.andrey77@gmail.com>
2022-08-12 11:39:02 -0400

    gh pages prep

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -57,7 +57,7 @@ const Projects = () => {
         {works.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
-              <img src={require(`../../assets/${work.imgSrc}.png`)} alt={work.name} />
+              <img src={require(`assets/${work.imgSrc}.png`)} alt={work.name} />
               <motion.div whileHover={{ opacity: [0, 1] }} transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }} className="app__work-hover app__flex">
                 <a href={work.sitelink} target="_blank" rel="noreferrer">
                   <motion.div whileInView={{ scale: [0, 1] }} whileHover={{ scale: [1, 0.90] }} transition={{ duration: 0.25 }} className="app__flex">
